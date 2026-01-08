5f5fb5dd386384a6b7df8dbab1200d53e164e0af
bea983 <beajanetordecilla@gmail.com>
2026-01-08 15:03:04 +0800

    Add files via upload

---
+++ lab-8-bea/lab-8/src/test/java/com/tordecilla/lab_8/Lab8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tordecilla.lab_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
